38afc8b9cf660a3b1064e9ebb3e94f0ec1c7898f
Fahmid Ahmed <bsse0921@iit.du.ac.bd>
2021-04-14 17:14:10 +0600

    bar chart updated

--- src/pages/Chart.vue
+++ src/pages/Chart.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="example">
+  <div v-if="isLoaded" class="example">
     <apexchart height="600" type="line" :options="chartOptions" :series="series"></apexchart>
   </div>
 </template>
 
 <script>
+import { api } from 'boot/axios'
 export default {
   name: 'AreaExample',
+  props: ['trading_code'],
   data: function() {
     return {
       chartOptions: {
@@ -17,7 +19,7 @@ export default {
             curve: 'smooth'
         },
         xaxis: {
-            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+            categories: [],
         },
         tooltip: {
             fixed: {
@@ -27,14 +29,65 @@ export default {
         }
       }
       ,
-      series: [{
+      series: [
+        {
           name: 'Actual Data',
-          data: [31, 40, 28, 51, 42, 109, 100]
-      }, {
+          data: []
+        },
+        {
           name: 'Predicted Data',
-          data: [33, 38, 35, 47, 38, 90, 91]
-      }],
+          data: []
+        }
+      ],
+      isLoaded: false
     }
   },
+  methods:{
+        calcultaeOHLC(company){
+        var o = parseFloat(company.yesterdays_closing_price);
+        var h = parseFloat(company.high);
+        var l = parseFloat(company.low);
+        var c = parseFloat(company.closing_price);
+        var ohlc = (o+h+l+c)/4;
+        ohlc = Math.round(ohlc*100)/100;
+        return ohlc;
+    },
+  },
+  mounted(){
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    api.get(`/companies/${this.trading_code}/data`)
+      .then((response) => {
+        if(response.status === 200){
+          var data = JSON.parse(response.data.data);
+          data = data.reverse();
+          //console.log(data);
+          var i = 0;
+          var pred = 0;
+          data.forEach(element => {
+            var date = new Date(element.fields.parsed_date);
+            //console.log(date);
+            if(i != 0)
+            {
+              this.chartOptions.xaxis.categories.push(date.getUTCDate()+"-"+months[date.getUTCMonth()]);
+              var ohlc = this.calcultaeOHLC(element.fields);
+              this.series[0].data.push(ohlc);
+              this.series[1].data.push(pred);
+              pred = parseFloat(element.fields.predicted_next_day_ohlc_price);
+            }
+            else
+            {
+              pred = parseFloat(element.fields.predicted_next_day_ohlc_price);
+            }
+            i++;
+
+          });
+          this.isLoaded = true;
+        }
+        else
+        {
+          console.log("data load failed");
+        }
+      })
+  }
 }
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -26,37 +26,30 @@
         @row-click="onRowClick"
         style="position:absolute;width:700px;"
         >
-        <template v-slot:header="props">
-        <q-tr :props="props">
-          <q-th v-for="col in props.cols" :key="col.name" :props="props">
-            {{ col.label }}
-            <p style="margin-top:0;" v-if="(col.label ==='Opening Price') || (col.label ==='Closing Price')">
-             {{ yes_date }}
-            </p>
-            <p style="margin-top:0;" v-if="(col.label ==='Predicted Closing Price')">
-             {{ cur_date }}
-            </p>
-            <p v-else></p>
-          </q-th>
-        </q-tr>
-      </template>
-        <!-- style="position:relative;width:100%" -->
-
-        <!-- <template v-if="!isAnalyticsHidden" v-slot:top-right>
-        <q-btn
-          color="secondary"
-          icon-right="analytics"
-          label="Generate Prediction"
-          no-caps
-          @click="addPredictedColumn"
-        />
-        </template> -->
-          <template v-slot:body-cell-actions="props">
-            <q-td :props="props">
-              <q-btn dense round flat color="grey" @click="deleteRow(props)" icon="remove_circle_outline"></q-btn>
-            </q-td>          
+          <template v-slot:header="props">
+            <q-tr :props="props">
+              <q-th v-for="col in props.cols" :key="col.name" :props="props">
+              {{ col.label }}
+              <p style="margin-top:0;" v-if="(col.label ==='Opening Price') || (col.label ==='Closing Price')">
+              {{ yes_date }}
+              </p>
+              <p style="margin-top:0;" v-if="(col.label ==='Predicted Market Change')">
+              {{ cur_date }}
+              </p>
+              <p v-else></p>
+              </q-th>
+            </q-tr>
           </template>
-
+          <template v-slot:body-cell-actions="props">
+              <q-td :props="props">
+                <q-btn dense round flat color="grey" @click="deleteRow(props)" icon="remove_circle_outline"></q-btn>
+              </q-td>         
+          </template>
+          <template v-slot:body-cell-predictedMarketChange="props">
+            <q-td :props="props" :class="props.row.predictedMarketChange.includes('-') ? 'text-red':'text-green'">
+              {{props.row.predictedMarketChange}}
+            </q-td>
+          </template>
         </q-table>
       </div>
       <div class="col-4 col-md q-ml-md">
@@ -68,7 +61,9 @@
       <q-card-section>
        <!-- <p> {{ company_details[0].name }} {{ company_details[0].value }}</p> -->
       <ul style="padding-left:8%">
-        <li v-for="item in company_details" v-bind:key="item.name"><b>{{ item.name }}</b> {{ item.value }}</li>
+        <li v-for="item in company_details" v-bind:key="item.name">
+            <b>{{ item.name }}</b> {{ item.value }}
+        </li>
       </ul>
       </q-card-section>
       <q-separator dark />
@@ -86,7 +81,8 @@
               <q-btn icon="close" flat round dense v-close-popup />
             </q-card-section>
             <q-card-section class="q-pt-none">
-              <Chart/>
+              <Chart :trading_code = "trading_code"
+              />
             </q-card-section>
             <q-card-actions align="right" class="bg-white text-teal">
               <q-btn flat label="OK" v-close-popup />
@@ -139,7 +135,7 @@ export default {
             name: value.name,
             openingPrice: value.yesterdays_closing_price,
             closingPrice: value.closing_price,
-            predictedPrice: value.predicted_next_day_ohlc_price
+            predictedMarketChange: value.marketChange+"%"
           })
         this.data.reverse()
         this.isAnalyticsHidden = false;
@@ -157,6 +153,7 @@ export default {
       //                        '-EMA26: ' + company[0].emA26 +
       //                        '-CMA: ' + company[0].cma +
       //                        '-MACD: ' + company[0].macd;
+      this.trading_code = company[0].trading_code;
       this.company_details = [{name: 'EMA12: ',value: company[0].emA12},
                              {name: 'EMA26: ', value: company[0].emA26},
                              {name: 'SMA: ', value: company[0].cma},
@@ -179,23 +176,39 @@ export default {
       })
       
 
+    },
+    calcultaeOHLC(company){
+        var o = parseFloat(company.yesterdays_closing_price);
+        var h = parseFloat(company.high);
+        var l = parseFloat(company.low);
+        var c = parseFloat(company.closing_price);
+        var ohlc = (o+h+l+c)/4;
+        return ohlc;
+    },
+    calcultaeMarketChange(company){
+        var marketChange =((parseFloat(company.predicted_next_day_ohlc_price) - company.ohlc)/company.ohlc)*100;
+        marketChange = Math.round(marketChange*100)/100;
+        return marketChange;
     }
+
 
   },
   created(){
-    // api.get('/predictions')
-    //   .then((response) => {
-    //     console.log(JSON.stringify(response.data))
-    //     if(response.status === 200){
-    //        this.companies = JSON.parse(response.data.data);
-    //        this.yes_date = new Date(this.companies[0].parsed_date).getDate();
-    //        this.cur_date = new Date(this.yes_date.getHours() + 24);
-    //     }
-    //     //this.companies = response.data
-    //   })
-    //   .catch((err) => {
-    //     alert(err)
-    //   })
+     /*api.get('/predictions')
+       .then((response) => {
+         console.log(JSON.stringify(response.data))
+         if(response.status === 200){
+            this.companies = JSON.parse(response.data.data);
+            var date = new Date(this.companies[0].parsed_date);
+            this.yes_date = date.getUTCDate() + '-' + (date.getUTCMonth()+1) + '-' + date.getUTCFullYear();
+            date = new Date(date.getTime() + 1*24*60*60*1000);
+            this.cur_date = date.getUTCDate() + '-' + (date.getUTCMonth()+1) + '-' + date.getUTCFullYear();
+         }
+         //this.companies = response.data
+       })
+       .catch((err) => {
+         alert(err)
+       })*/
 
   },
   mounted(){
@@ -203,6 +216,12 @@ export default {
     this.yes_date = date.getUTCDate() + '-' + (date.getUTCMonth()+1) + '-' + date.getUTCFullYear();
     date = new Date(date.getTime() + 1*24*60*60*1000);
     this.cur_date = date.getUTCDate() + '-' + (date.getUTCMonth()+1) + '-' + date.getUTCFullYear();
+
+    this.companies.forEach((company) => {
+        company.ohlc = this.calcultaeOHLC(company);
+        company.marketChange = this.calcultaeMarketChange(company);
+    });
+
     // this.cur_date = new Date(this.yes_date.getDays() + 1);
   },
   data(){
@@ -210,6 +229,7 @@ export default {
       cur_date : '10/03/2021',
       yes_date: '09/03/2021',
       company_title:'',
+      trading_code: '',
       company_details:[{name: 'Select a company from the table to see more details!', value: null}],
       isHidden: true,
       isAnalyticsHidden: true,
@@ -228,7 +248,13 @@ export default {
         },
         { name: 'openingPrice', align: 'center', label: 'Opening Price', field: 'openingPrice', sortable: true, headerStyle: 'width: 110px' },
         { name: 'closingPrice', align: 'center', label: 'Closing Price', field: 'closingPrice', sortable: true },
-        { name: 'predictedPrice', align: 'center', label: 'Predicted Closing Price', field: 'predictedPrice', sortable: true }
+        { 
+          name: 'predictedMarketChange',
+          align: 'center',
+          label: 'Predicted Market Change', 
+          field: 'predictedMarketChange',
+          sortable: true 
+        }
       ],
       data: [
         
@@ -251,17 +277,19 @@ export default {
           emA12: 1,
           emA26: 2,
           cma: 3,
-          macd: 4
+          macd: 4,
+          ohlc: 0,
+          marketChange: 0
         },
         {
-          trading_code: "1JANATAMF",
+          trading_code: "AAMRANET",
           parsed_date: "2021-04-12T18:00:00.000+00:00",
           last_traded_price: "5.100",
           high : "5.100",
           low: "4.900",
           closing_price: "4.900",
           predicted_next_day_ohlc_price: "4.993",
-          yesterdays_closing_price:"5.100",
+          yesterdays_closing_price:"4.5",
           change: "4.900",
           trade: 67,
           value_mn: "1.391",
@@ -270,10 +298,12 @@ export default {
           emA12: 1,
           emA26: 2,
           cma: 3,
-          macd: 4
+          macd: 4,
+          ohlc: 0,
+          marketChange: 0
         },
         {
-          trading_code: "1JANATAMF",
+          trading_code: "AAMRATECH",
           parsed_date: "2021-04-12T18:00:00.000+00:00",
           last_traded_price: "5.100",
           high : "5.100",
@@ -289,10 +319,12 @@ export default {
           emA12: 1,
           emA26: 2,
           cma: 3,
-          macd: 4
+          macd: 4,
+          ohlc: 0,
+          marketChange: 0
         },
         {
-          trading_code: "1JANATAMF",
+          trading_code: "ABB1STMF",
           parsed_date: "2021-04-12T18:00:00.000+00:00",
           last_traded_price: "5.100",
           high : "5.100",
@@ -308,7 +340,9 @@ export default {
           emA12: 1,
           emA26: 2,
           cma: 3,
-          macd: 4
+          macd: 4,
+          ohlc: 0,
+          marketChange: 0
         }, 
       ]
     }
